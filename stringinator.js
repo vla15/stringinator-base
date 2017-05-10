14d558edd2470448181a1bd5ce93e8999cc6e67d
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-05-09 17:33:11 -0700

    updating stringinator functions

--- stringinator.js
+++ stringinator.js
@@ -1,39 +1,41 @@
 const _ = require('./underbar');
 
 const first = function(str, n) {
-  // Your code goes here
+  return _.first(str, n);
 };
 
 const last = function(str, n) {
-  // Your code goes here
+  return _.last(str, n);
 };
 
 const removeChar = function(str, target) {
-  // hint: use _.reject
-  // Your code goes here
+  return _.reject(str, item => item === target).join('');
 };
 
 const hasChar = function(str, target) {
-  // hint: use _.some
-  // Your code goes here
+  return _.some(str, item => target === item);
 };
 
 const isOnlyDigits = function(str) {
-  // Your code goes here
+  return _.every(str, num => !isNaN(num));
 };
 
 const filterToOnlyDigits = function(str) {
-  // Your code goes here
+  return _.filter(str, num => parseInt(num)).join('');
 };
 
 const truncateString = function(val, maxLength) {
-  // A freebie solution, this is the ONLY method here that doesn't use Underbar.
   return String(val).slice(0, maxLength);
 };
 
 const truncateLongItems = function(obj, maxLength) {
-  // hint: use truncateString above
-  // Your code goes here
+  if (typeof obj === 'string') {
+    return truncateString(obj, maxLength);
+  } else {
+    return _.map(obj, item => {
+      return truncateString(item, maxLength);
+    });
+  }
 };
 
 const countChars = function(str) {
